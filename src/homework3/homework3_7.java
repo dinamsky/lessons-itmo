194bad92c9b298e545a3ce052f82af1959f16fbd
dinamsky <dinamsky@gmail.com>
2019-03-15 17:31:56 +0300

    ok

--- src/homework3/homework3_7.java
+++ src/homework3/homework3_7.java
@@ -1,25 +1,35 @@
 package homework3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class homework3_7 {
     public static void main(String[] args) {
         System.out.println("Задача 7 поиск в строке чисел и запись в числовой массив");
         String stringOrigin = "дом 48, корпус 9, парадная 7, этаж 4";
-        char[] num = {0,1,2,3,4,5,6,7,8,9};
-        int pos = 0;
-        char[] chars = stringOrigin.toCharArray();
-
+//        String stringOrigin = args[0];
+        String digitStr = "";
+        List<Integer> digits = new ArrayList<Integer>();
 
         for (int i = 0; i < stringOrigin.length(); i++) {
-          for(int k=0;k<10;k++){
-              if (chars[i]==num[k]) {
-
-
-              }
-          }
+            if (Character.isDigit(stringOrigin.charAt(i))) {
+                digitStr += stringOrigin.charAt(i);
+            } else {
+                if (!digitStr.isEmpty()) {
+                    digits.add(Integer.parseInt(digitStr));
+                    digitStr = "";
+                }
+            }
+        }
+        if (!digitStr.isEmpty()) {
+            digits.add(Integer.parseInt(digitStr));
+            digitStr = "";
         }
 
+        for (Integer i : digits) {
+            System.out.println(i);
+        }
 
     }
 
