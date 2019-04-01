10fe6510485915fc8ea9ae35ff6ad840f7e481da
dinamsky <dinamsky@gmail.com>
2019-04-01 14:38:46 +0300

    обновлено д.з. 10 го урока. War and Peace

--- src/lesson10/text/TextStat.java
+++ src/lesson10/text/TextStat.java
@@ -1,17 +1,15 @@
 package lesson10.text;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 
 
-    public class TextStat
-    {
-        public static void main(String[] args) throws IOException {
+public class TextStat {
+        public static void main(String[] args) throws IOException  {
             String fileName = "C:\\Users\\Игорь\\IdeaProjects\\lessons\\resources\\wp.txt";
             String str = Files.readString(Paths.get(fileName));
         //    System.out.println(str);
@@ -19,7 +17,7 @@ import java.util.Map;
             str = str.replaceAll("\\W", " ");
             System.out.println(str);
             Map<String, Integer> map = new HashMap<>();
-          int n = 1;
+
             for(String s: str.split(" ")) {
                 if(map.containsKey(s)) {
                     map.put(s, map.get(s) + 1);
@@ -27,6 +25,19 @@ import java.util.Map;
                 else {
 
                     map.put(s, 1);
+
+                }
+
+            }
+            Map<String, Integer> map5 = new HashMap<>();
+
+            for(String s: str.split(" ")) {
+                if(map5.containsKey(s)) {
+                    map5.put(s, map5.get(s) + 1);
+                }
+                else {
+
+                    map5.put(s, 1);
 
                 }
 
@@ -42,10 +53,48 @@ import java.util.Map;
                 }
                 list2.add(word);
             }
+            Map<Character,Integer> map3 = new HashMap<>();
+            char[] mass = str.replaceAll("\\s+","").toCharArray();
+            for (int i = 0; i<mass.length; i++){
+                char lett = mass[i];
+                if(map3.containsKey(lett)) {
+
+                    map3.put(lett, map3.get(lett) + 1);
+                }
+                else {
+
+                    map3.put(lett, 1);
+
+                }
+
+            }
+            Map<String, Integer> newMap = new TreeMap(Collections.reverseOrder());
+
+            map.entrySet().stream().sorted(MyComparator.comparing(Map.Entry::getValue))
+                    .forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+            map3.entrySet().stream().sorted(MyComparator.comparing(Map.Entry::getValue))
+                    .forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
 
             map.remove("");
             System.out.println(map);
-            System.out.println(map2);
+          //  System.out.println(map2);
+           // System.out.println(map3);
         }
     }
+class MyComparator implements Comparator {
+    Map map;
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+
+    public static <T, U extends Comparable<? super U>> Comparator<T> comparing(
+            Function<? super T, ? extends U> keyExtractor)
+    {
+        Objects.requireNonNull(keyExtractor);
+        return (Comparator<T> & Serializable)
+                (c1, c2) -> keyExtractor.apply(c2).compareTo(keyExtractor.apply(c1));
+    }
+}
 
